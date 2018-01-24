a7c78571ef875cc65089e1291fbe753ab8105762
Alana Kamahele <kamahele@usc.edu>
2018-01-24 15:11:46 -0800

    Initial Commit

---
+++ Warrior/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Warrior
+//
+//  Created by Alana Kamahele on 1/24/18.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Warrior/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Warrior
+//
+//  Created by Alana Kamahele on 1/24/18.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
